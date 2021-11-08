b95e8fd07083c51a510a836670f4c0846c239ea2
Per Kraulis <per.kraulis@scilifelab.se>
2021-11-08 14:25:07 +0100

    Upgraded, use CouchDB2 dump/undump. Closes #248. Closes #247. Closes #246.

--- datagraphics/cli.py
+++ datagraphics/cli.py
@@ -61,70 +61,11 @@ def execute(pargs):
             filepath = "dump_{}.tar.gz".format(time.strftime("%Y-%m-%d"))
         else:
             filepath = pargs.dump
-        dump(flask.g.db, filepath)
+        ndocs, nfiles = flask.g.db.dump(filepath, exclude_designs=True)
+        print(f"dumped {ndocs} documents and {nfiles} files to {filepath}")
     elif pargs.load:
-        load(flask.g.db, pargs.load)
-
-def dump(db, filepath):
-    if filepath.endswith(".gz"):
-        mode = "w:gz"
-    else:
-        mode = "w"
-    outfile = tarfile.open(filepath, mode=mode)
-    count_items = 0
-    count_files = 0
-    for doc in db:
-        # Only documents that explicitly belong to the application
-        if doc.get("doctype") is None: continue
-        info = tarfile.TarInfo(doc["_id"])
-        rev = doc.pop("_rev")
-        data = json.dumps(doc).encode("utf-8")
-        doc["_rev"] = rev
-        info.size = len(data)
-        outfile.addfile(info, io.BytesIO(data))
-        count_items += 1
-        for attname in doc.get("_attachments", dict()):
-            info = tarfile.TarInfo("{0}_att/{1}".format(doc["_id"], attname))
-            attfile = db.get_attachment(doc, attname)
-            if attfile is None: continue
-            data = attfile.read()
-            attfile.close()
-            info.size = len(data)
-            outfile.addfile(info, io.BytesIO(data))
-            count_files += 1
-    outfile.close()
-    print(f"dumped {count_items} items and {count_files} files to {filepath}")
-
-def load(db, filepath):
-    "Load (undump) data from a tar.gz file. The database should be empty."
-    count_items = 0
-    count_files = 0
-    attachments = dict()
-    infile = tarfile.open(filepath, mode="r")
-    for item in infile:
-        itemfile = infile.extractfile(item)
-        itemdata = itemfile.read()
-        itemfile.close()
-        print(item.name)
-        if item.name in attachments:
-            # This relies on an attachment being after its item in the tarfile.
-            db.put_attachment(doc, itemdata, **attachments.pop(item.name))
-            count_files += 1
-        else:
-            doc = json.loads(itemdata)
-            # If the account already exists, do not load document.
-            if doc["doctype"] == constants.DOCTYPE_USER:
-                rows = db.view("users", "username", key=doc["username"])
-                if len(list(rows)) != 0: continue
-            atts = doc.pop("_attachments", dict())
-            db.put(doc)
-            count_items += 1
-            for attname, attinfo in atts.items():
-                key = "{0}_att/{1}".format(doc["_id"], attname)
-                attachments[key] = dict(filename=attname,
-                                        content_type=attinfo["content_type"])
-    infile.close()
-    print(f"loaded {count_items} items and {count_files} files from {filepath}")
+        ndocs, nfiles = flask.g.db.undump(pargs.load)
+        print(f"loaded {ndocs} documents and {nfiles} files from {pargs.load}")
 
 def main():
     "Entry point for command line interface."
