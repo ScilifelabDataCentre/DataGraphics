293288da387b824b708627200ecd8bd9bbb4e872
Per Kraulis <per.kraulis@gmail.com>
2021-12-02 11:50:34 +0100

    Put URLs into Constants. Closes #264.

--- datagraphics/__init__.py
+++ datagraphics/__init__.py
@@ -4,7 +4,7 @@ import re
 import os.path
 import string
 
-__version__ = "0.12.4"
+__version__ = "0.12.5"
 
 class Constants:
     VERSION = __version__
@@ -22,6 +22,10 @@ class Constants:
     JQUERY_URL = "https://jquery.com/"
     JQUERY_JS_URL = "https://code.jquery.com/jquery-3.5.1.slim.min.js"
     JQUERY_JS_INTEGRITY = "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+
+    JQUERY_LOCALTIME_URL = 'https://plugins.jquery.com/jquery.localtime/'
+    JQUERY_LOCALTIME_VERSION = '0.9.1'
+    JQUERY_LOCALTIME_FILENAME = 'jquery.localtime-0.9.1.min.js'
 
     DATATABLES_VERSION = "1.10.24"
     DATATABLES_URL = "https://datatables.net/"
